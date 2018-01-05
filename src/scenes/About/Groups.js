5fb5d61dcb17aa908b565b05b6ab8b4098b5acd4
saurabh303 <saurabhk.singh.cse16@itbhu.ac.in>
2018-01-05 20:27:54 +0530

    [saurabh303] added Group/TeamMembers component.

---
+++ src/scenes/About/Groups.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import GroupsComponent from './components/GroupsComponent';
+
+class Groups extends Component{
+ 
+    render() {
+        return (
+            <div>
+                <GroupsComponent/>
+            </div>
+        );
+    }
+}
+
+export default Groups;
